d94ef44224f799d52161815028c1caa074a24446
Marianna <mddobal@gmail.com>
2019-07-14 15:56:38 -0300

    Updates

---
+++ precache-manifest.52865917559b54f79ae825932126a8c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4fb890b373a11dd8ae24b8227897bb",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "9e133f117bc037c0e405",
+    "url": "/starwars/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "43c6a86a3006fa245a29",
+    "url": "/starwars/static/js/2.7bfa3016.chunk.js"
+  },
+  {
+    "revision": "9e133f117bc037c0e405",
+    "url": "/starwars/static/js/main.1fd5066d.chunk.js"
+  },
+  {
+    "revision": "9cd712b42b70d05ab420",
+    "url": "/starwars/static/js/runtime~main.f4b6364c.js"
+  }
+]);
